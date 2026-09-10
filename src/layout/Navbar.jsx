4bbed322e8daf4b276b16d7e9edfdf5e75f1d479
George <gl8520@students.cs.ubc.ca>
2026-09-09 21:35:23 -0700

    first commit

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,68 @@
+import { Button } from "@/components/Button";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
+const navLinks = [
+    { href: "#about", label: "About" },
+    { href: "#projects", label: "Projects" },
+];
+
+
+export const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    return (
+        <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+            <nav className="container mx-auto px-6 flex items-center justify-between">
+
+                <a href="#" 
+                className="text-xl font-bold tracking-tight hover:text-primary">
+                    GL<span className="text-primary">.</span>
+                </a>
+
+                <div className="hidden md:flex items-center gap-1">
+                    <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
+                        {navLinks.map((link, index) => (
+                            <a href={link.href} 
+                            key={index} 
+                            className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface">
+                                {link.label}
+                                </a>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="hidden md:block">
+                    <Button size="sm">
+                        Contact Me
+                    </Button>
+                </div>
+
+                {/* Mobile Menu Button */}
+                <button className="md:hidden p-2 text-foreground cursor-pointer" 
+                onClick={() => setIsMobileMenuOpen((prev) => !prev)}>
+                    {isMobileMenuOpen ? <X size={24}/> : <Menu size={24} />}
+                </button>
+
+            </nav>
+
+            {/* Mobile Menu */}
+            {isMobileMenuOpen && (
+                <div className="md-hidden glass-strong animate-fade-in">
+                <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+                    {navLinks.map((link, index) => (
+                        <a href={link.href} 
+                        key={index} 
+                        className="text-lg text-muted-foreground hover:text-foreground py-2">
+                            {link.label}
+                            </a>
+                    ))}
+
+                    <Button>
+                        Contact Me
+                    </Button>
+                </div>
+            </div>
+            )}
+        </header>
+    );
+};
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,8 @@
+export const Hero = () => {
+    return <section className="relative min-h-screen flex items-center overflow-hidden">
+        {/* Background */}
+        <div>
+            <img />
+        </div>
+    </section>;
+}
